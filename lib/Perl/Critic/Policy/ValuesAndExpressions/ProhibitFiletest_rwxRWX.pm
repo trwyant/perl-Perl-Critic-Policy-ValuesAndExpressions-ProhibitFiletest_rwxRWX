62c6c515093ccc10adac79005d482d1269e86711
Tom Wyant <harryfmudd@comcast.net>
2026-06-12 08:34:48 -0400

    Version 0.003
    Set version to 0.003. Update Changes file for changes since v00.002_01.

--- lib/Perl/Critic/Policy/ValuesAndExpressions/ProhibitFiletest_rwxRWX.pm
+++ lib/Perl/Critic/Policy/ValuesAndExpressions/ProhibitFiletest_rwxRWX.pm
@@ -15,7 +15,7 @@ use Perl::Critic::Utils qw{ :severities :classification :ppi :booleans
 use PPI;
 use PPI::Document;
 
-our $VERSION = '0.002_01';
+our $VERSION = '0.003';
 
 #-----------------------------------------------------------------------------
 
